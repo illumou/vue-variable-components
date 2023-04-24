05fd6c1bb71037e3915d234d6d7165764c366c33
illumou <82582947+illumou@users.noreply.github.com>
2023-04-24 08:52:52 +0200

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,14 @@
 <template>
   <div class="h-screen w-full bg-[#0d0d0d] flex items-center justify-center flex-col space-y-4">
     <div class="flex items-center justify-center flex-col w-full space-y-2">
-      <div class="w-1/3 space-x-10 flex flex-row justify-between pb-2 border-b-2 border-white">
+      <div class="w-1/2 space-x-10 flex flex-row justify-between pb-2 border-b-2 border-white">
         <h1 class="text-white text-2xl"> Components: </h1>
         <div class="space-x-3">
           <button class="border-2 border-white p-2 bg-white rounded-xl hover:shadow-amber-100" @click="changeComponent('one')"> Component 1 </button>
           <button class="border-2 border-white p-2 bg-white rounded-xl hover:shadow-amber-100" @click="changeComponent('two')"> Component 2 </button>
         </div>
       </div>
-      <div class="w-1/3 space-x-10 flex flex-row justify-between pb-2 border-b-2 border-white">
+      <div class="w-1/2 space-x-10 flex flex-row justify-between pb-2 border-b-2 border-white">
         <h1 class="text-white text-2xl"> Background-color: </h1>
         <div class="space-x-3">
           <button class="border-2 border-white p-2 bg-white rounded-xl" @click="changeColor('default')"> Default </button>
@@ -17,7 +17,7 @@
           <button class="border-2 border-white p-2 bg-white rounded-xl" @click="changeColor('blue')"> Blue </button>
         </div>
       </div>
-      <div class="w-1/3 space-x-10 flex flex-row justify-between pb-2 border-b-2 border-white">
+      <div class="w-1/2 space-x-10 flex flex-row justify-between pb-2 border-b-2 border-white">
         <h1 class="text-white text-2xl"> Custom background-color: </h1>
         <div class="space-x-1">
           <input class="h-full rounded-xl text-center" v-model="inputColor" placeholder="custom color" @keypress.enter="changeColor(inputColor)"/>
@@ -25,7 +25,7 @@
         </div>
       </div>
     </div>
-    <div class="w-1/3 h-96 border-2 border-white flex items-center justify-center">
+    <div class="w-1/2 h-96 border-2 border-white flex items-center justify-center">
       <h1 v-if="!componentActive" class="text-white font-bold text-2xl"> NIX HIER</h1>
       <component :is="current" :bgColor="bgColor" v-if="componentActive" @closeComponent="closeComponent"/>
     </div>
