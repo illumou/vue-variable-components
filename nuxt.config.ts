d3fcbe39eed7ddcdd6e7c532fc2e42c5e291ccb0
Robin <82582947+illumou@users.noreply.github.com>
2023-04-26 17:20:14 +0200

    Changes
    Change to nuxt config and build.yml

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
     // @ts-ignore
+    target: 'static',
+    router: {
+        base: '/vue-variable-components'
+    },
     css: ['~/assets/css/tailwind.css'],
     postcss: {
         plugins: {
