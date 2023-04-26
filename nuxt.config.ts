5334bc9f58f672d06aeb22be8e30cdda6a99b90b
Robin <82582947+illumou@users.noreply.github.com>
2023-04-26 17:32:18 +0200

    changes

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
     // @ts-ignore
-    target: 'static',
-    router: {
-        base: '/vue-variable-components'
+    app: {
+        baseURL: '/vue-variable-components/'
     },
     css: ['~/assets/css/tailwind.css'],
     postcss: {
